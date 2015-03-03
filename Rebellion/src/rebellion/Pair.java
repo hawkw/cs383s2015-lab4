80a884774cadd032058dabd5993cd321ef3267f0
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-03 13:28:45 -0500

    Add documentation to learning components (Closes #6)

--- Rebellion/src/rebellion/Pair.java
+++ Rebellion/src/rebellion/Pair.java
@@ -1,14 +1,46 @@
 package rebellion;
 
+/**
+ * Holds an observed number of active agents associated with a positive or
+ * negative reinforcement.
+ * 
+ * This class is only necessary because Java doesn't have tuples.
+ *
+ * Instances of <code>Pair</code> are immutable and cannot be modified.
+ * 
+ * @author Hawk Weisman <hawk@meteorcodelabs.com>
+ *
+ */
 public class Pair {
-	private int numCops;
-	private double weight;
-	
-	public Pair(int numCops, double weight) {
-		this.numCops = numCops;
-		this.weight = weight;
+	private int numActive;
+	private double reinforcement;
+
+	/**
+	 * Create a new <code>Pair</code> with the specified number of active agents
+	 * and reinforcement value.
+	 * 
+	 * @param numActive
+	 *            the observed number of active agents
+	 * @param reinforcement
+	 *            the positive or negative reinforcement
+	 */
+	public Pair(int numActive, double reinforcement) {
+		this.numActive = numActive;
+		this.reinforcement = reinforcement;
 	}
-	
-	public int getNumCops () { return this.numCops; }
-	public double getWeight ()  { return this.weight;  }
+
+	/**
+	 * @return the number of active agents at the time of this observation
+	 */
+	public int getNumActive() {
+		return this.numActive;
+	}
+
+	/**
+	 * @return the positive or negative reinforcement value associated with this
+	 *         observation.
+	 */
+	public double getReinforcement() {
+		return this.reinforcement;
+	}
 }
--- Rebellion/src/rebellion/Person.java
+++ Rebellion/src/rebellion/Person.java
@@ -18,55 +18,75 @@ import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.grid.Grid;
 import repast.simphony.space.grid.GridPoint;
 import repast.simphony.util.SimUtilities;
+
 /**
- * @author grace
- * Person class
+ * @author grace Person class
  */
 public class Person implements Agent {
-	
-	private static int NUM_NEIGHBORS = 50;
-	
-	private double knearest(int target) {
-		Set<Pair> neighbors = new HashSet<>();
-		
-		for (int i = 0; i <= NUM_NEIGHBORS; i++) {
-			Pair best = null;
-			Integer bestDist = null;
-			for (Pair n : this.prevNumActive ) {
-				if (
-						(best == null && bestDist == null) || 
-						((Math.abs(target - n.getNumCops()) < bestDist) && !neighbors.contains(n)) ) {
-					best = n;
-					bestDist = Math.abs(target - n.getNumCops());
+
+	private static int NUM_NEIGHBORS = 50; // K value for K-nearest neighbor
+											// search
+
+	/**
+	 * Search this agent's memory of previously-observed numbers of nearby
+	 * active agents for the <i>K</i> nearest neighbors to the
+	 * <code>target</code> value, and return the sum of the reinforcements for
+	 * those observations.
+	 * 
+	 * @param target
+	 *            the target number to find the nearest neighbors for
+	 * @return the sum of the <i>K</i> nearest neighbors to the target value.
+	 */
+	private double kNearest(int target) {
+		Set<Pair> neighbors = new HashSet<>(); // the nearest neighbors to the
+												// target
+
+		for (int i = 0; i <= NUM_NEIGHBORS; i++) { // repeat search K times
+			Pair best = null; // current nearest neighbor
+			Integer bestDist = null; // distance to the nearest neighbor. This
+										// is a big-I integer so that it can be
+										// nullable
+			for (Pair n : this.prevNumActive) { // for each previous observation
+				if (	// if we haven't found a best neighbor yet...
+						(best == null && bestDist == null) 
+						// or if the current distance is smaller than the previous best
+						|| ((Math.abs(target - n.getNumActive()) < bestDist)
+						// and we haven't already marked this item as a neighbor
+						&& !neighbors.contains(n))
+					) {
+					best = n; // the current element is the nearest neighbor
+					bestDist = Math.abs(target - n.getNumActive()); // update the best distance
 				}
 			}
 			neighbors.add(best);
 		}
 		double sum = 0.0;
+		// return the sum of the rewards for each neighbor
 		for (Pair n : neighbors) {
-			sum += n.getWeight();
+			sum += n.getReinforcement();
 		}
 		return sum;
 	}
 
-	private Grid <Object> grid;
-	private ContinuousSpace <Object> space;
-	
+	private Grid<Object> grid;
+	private ContinuousSpace<Object> space;
+
 	private List<Pair> prevNumActive;
-	
+
 	private double riskAversion; // fixed for lifetime; 0 to 1
 	private double perceivedHardship; // 0 to 1
 	public boolean active; // true if currently rebelling
-	public int jailTerm; // remaining jail turns (if 0, then agent is not in jail)
+	public int jailTerm; // remaining jail turns (if 0, then agent is not in
+							// jail)
 	private int visNeighbors; // neighbor visibility radius range
 	public int MAX_JAIL_TERM;
 	private double grievance; // person's grievance
 	private double govLegitimacy; // user-specified government legitimacy
 	private double arrestProb; // person's arrest probability
-	public int red;   // assign color base on behavior
+	public int red; // assign color base on behavior
 	public int green;
 	public int blue;
-	
+
 	public Person(Grid<Object> grid, ContinuousSpace<Object> space,
 			int visNeighbors, int maxJailTerm, double govLegitimacy) {
 		this.grid = grid;
@@ -74,7 +94,8 @@ public class Person implements Agent {
 		this.visNeighbors = visNeighbors;
 		this.MAX_JAIL_TERM = maxJailTerm;
 		this.riskAversion = Constants.RISK_AVERSION; // fixed for lifetime
-		this.perceivedHardship = Constants.PERCEIVED_HARDSHIP; // fixed for lifetime
+		this.perceivedHardship = Constants.PERCEIVED_HARDSHIP; // fixed for
+																// lifetime
 		this.active = false; // by default not rebelling
 		this.jailTerm = 0;
 		this.govLegitimacy = govLegitimacy; // user-specified
@@ -83,77 +104,81 @@ public class Person implements Agent {
 		set_colors();
 	}
 
-	/*
+	/**
 	 * Calculate grievance
 	 */
 	private void calc_grievance() {
 		this.grievance = this.perceivedHardship * (1 - this.govLegitimacy);
 	}
 
-	
-	/*
+	/**
 	 * Calculate arrest probability
+	 * @note this has been modified to use the learning algorithm we have implemented
+	 * @see Person#kNearest(int)
 	 */
-	private void est_arrest_prob(List<GridCell<Person>> personNeighborhood, 
+	private void est_arrest_prob(List<GridCell<Person>> personNeighborhood,
 			List<GridCell<Cop>> copNeighborhood) {
-		
+
 		int cCount = 0; // total cops
 		int pTotal = 0; // total people
 		int pJailedCount = 0;
 		int pActiveCount = 0; // total people who are active
-		
+
 		// count cops
 		for (GridCell<Cop> c : copNeighborhood) {
 			cCount += c.size();
 		}
-		
+
 		// count people
 		for (GridCell<Person> p : personNeighborhood) {
 			pTotal += p.size();
-			Object obj = grid.getObjectAt(p.getPoint().getX(), p.getPoint().getY());
+			Object obj = grid.getObjectAt(p.getPoint().getX(), p.getPoint()
+					.getY());
 			if (obj instanceof Person) {
-				if (((Person)obj).active) {
+				if (((Person) obj).active) {
 					pActiveCount++;
-				} else if (((Person)obj).jailTerm > 0) {
+				} else if (((Person) obj).jailTerm > 0) {
 					pJailedCount++;
 				}
 			}
 		}
-		
+
 		if (Constants.DEBUG) {
-			//System.out.println(location.toString()+ " visNeighbors "+visNeighbors);
-			System.out.println("active people nearby "+pActiveCount);
-			System.out.println("cops nearby "+cCount);
+			// System.out.println(location.toString()+
+			// " visNeighbors "+visNeighbors);
+			System.out.println("active people nearby " + pActiveCount);
+			System.out.println("cops nearby " + cCount);
 		}
-		
+
 		// calculate arrest probability
-		//this.arrestProb = 1 - Math.exp(-Constants.K * Math.floor(cCount/pActiveCount));
+		// this.arrestProb = 1 - Math.exp(-Constants.K *
+		// Math.floor(cCount/pActiveCount));
 		if (prevNumActive.isEmpty()) {
 			this.arrestProb = 0;
 		} else {
-			this.arrestProb = (double)knearest(pActiveCount);
+			this.arrestProb = (double) kNearest(pActiveCount);
 		}
-		
-		//if (Constants.DEBUG)
-		//	System.out.println("arrestProb "+arrestProb+" cCount "+cCount+" pActiveCount "+pActiveCount);
+
+		// if (Constants.DEBUG)
+		// System.out.println("arrestProb "+arrestProb+" cCount "+cCount+" pActiveCount "+pActiveCount);
 	}
 
-	/* 
+	/*
 	 * set colors base on person's current behavior
-	 * */
+	 */
 	private void set_colors() {
-		if (this.active==true) { // red
+		if (this.active == true) { // red
 			this.red = 0xFF;
 			this.green = 0x0;
 			this.blue = 0x0;
-		} else if (this.jailTerm>0) { // jailed
+		} else if (this.jailTerm > 0) { // jailed
 			this.red = 0x0;
 			this.green = 0x0;
 			this.blue = 0x0;
 		} else {
 			// grievance is between 0 and 1
 			this.red = 0x0;
-			this.green = 0xFF; //(int) (this.grievance * 0xFF);
+			this.green = 0xFF; // (int) (this.grievance * 0xFF);
 			this.blue = 0x0;
 		}
 	}
@@ -167,20 +192,21 @@ public class Person implements Agent {
 		GridPoint location = grid.getLocation(this);
 		// people nearby
 		List<GridCell<Person>> personNeighborhood = new GridCellNgh<Person>(
-				grid, location, Person.class, visNeighbors, 
-				visNeighbors).getNeighborhood(false);
-		
-		GridCellNgh<Cop> copNgh = new GridCellNgh<Cop>(grid, location, Cop.class,
-				visNeighbors, visNeighbors);
+				grid, location, Person.class, visNeighbors, visNeighbors)
+				.getNeighborhood(false);
+
+		GridCellNgh<Cop> copNgh = new GridCellNgh<Cop>(grid, location,
+				Cop.class, visNeighbors, visNeighbors);
 		List<GridCell<Cop>> copNeighborhood = copNgh.getNeighborhood(false);
-		
+
 		int pActiveCount = 0; // total people who are active
-	
+
 		// count people
 		for (GridCell<Person> p : personNeighborhood) {
-			Object obj = grid.getObjectAt(p.getPoint().getX(), p.getPoint().getY());
+			Object obj = grid.getObjectAt(p.getPoint().getX(), p.getPoint()
+					.getY());
 			if (obj instanceof Person) {
-				if (((Person)obj).active) {
+				if (((Person) obj).active) {
 					pActiveCount++;
 				}
 			}
@@ -196,46 +222,44 @@ public class Person implements Agent {
 
 			// look for empty cells
 			List<GridCell<Person>> freeCells = SMUtils
-				.getFreeGridCells(personNeighborhood);
-			
+					.getFreeGridCells(personNeighborhood);
+
 			if (freeCells.isEmpty()) {
 				return;
 			}
-			
+
 			// move to empty cell
 			SimUtilities.shuffle(freeCells, RandomHelper.getUniform());
-			GridCell<Person> chosenFreeCell = SMUtils.randomElementOf(freeCells);
+			GridCell<Person> chosenFreeCell = SMUtils
+					.randomElementOf(freeCells);
 
 			GridPoint newGridPoint = chosenFreeCell.getPoint();
 			grid.moveTo(this, newGridPoint.getX(), newGridPoint.getY());
-			
+
 			// should this person be active (rebel)?
 			// calculate grievance and arrest probability
 			this.calc_grievance();
 			this.est_arrest_prob(personNeighborhood, copNeighborhood);
-			
+
 			if (((this.grievance + this.riskAversion) * this.arrestProb) < Constants.THRESHOLD) {
 				this.active = true;
 				if (Constants.DEBUG)
-					System.out.println("ACTIVE grievance "+grievance+
-						" riskAversion "+riskAversion+
-						" arrestProb "+arrestProb+
-						" threshold "+Constants.THRESHOLD);
+					System.out.println("ACTIVE grievance " + grievance
+							+ " riskAversion " + riskAversion + " arrestProb "
+							+ arrestProb + " threshold " + Constants.THRESHOLD);
 			}
-			
+
 		} else if (this.jailTerm > 0) {
 			if (this.jailTerm == // if I was just arrested
-					(Integer)RunEnvironment
-						.getInstance()
-						.getParameters()
-						.getValue("maxJailTerm")
-						) this.prevNumActive.add(new Pair(pActiveCount, 0.1));	
+			(Integer) RunEnvironment.getInstance().getParameters()
+					.getValue("maxJailTerm"))
+				this.prevNumActive.add(new Pair(pActiveCount, 0.1));
 			this.jailTerm--; // decrement jail turn by 1 each step
 
 		}
-		
+
 		set_colors();
-		
+
 		if (Constants.DEBUG)
 			print_state();
 	}
@@ -244,7 +268,8 @@ public class Person implements Agent {
 	 * Print current state
 	 */
 	public void print_state() {
-		System.out.println("active? "+active+" G "+grievance+" A "+arrestProb);
+		System.out.println("active? " + active + " G " + grievance + " A "
+				+ arrestProb);
 	}
 
 	/*
